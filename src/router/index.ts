4238ece22aeacf2ee550d178c309278373bbb96d
crazwade <jackywu526@gmail.com>
2023-03-08 11:19:19 +0800

    feat: 新增登入路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Layout from "../layout/LayoutView.vue";
+import Login from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,22 @@ const router = createRouter({
       path: "/",
       component: Layout,
     },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
   ],
 });
+
+// 每次登入檢查
+// router.beforeEach((to, from, next) => {
+//   if (to.path !== "/login") {
+//     // 如果不是前往登入頁，則將使用者導向登入頁
+//     next("/login");
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
