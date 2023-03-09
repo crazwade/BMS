4d6622e81a4f4edf34e5a534d0e930fe66adffd4
crazwade <jackywu526@gmail.com>
2023-03-09 10:44:20 +0800

    feat: login 移至 view/login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Layout from "../layout/LayoutView.vue";
-import Login from "../views/LoginView.vue";
+import Login from "../views/login/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
