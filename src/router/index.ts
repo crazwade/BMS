281a5b98ca29cea5ab8458922613d61252a2aed8
crazwade <jackywu526@gmail.com>
2023-03-10 09:29:16 +0800

    feat: router 添加 views

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,13 +6,56 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      component: Layout,
-    },
-    {
       path: "/login",
       name: "Login",
       component: Login,
+    },
+    {
+      path: "/",
+      component: Layout,
+      name: "Main",
+      children: [
+        {
+          path: "attendanceRecords",
+          name: "AttendanceRecords",
+          component: () =>
+            import("../views/attendanceRecords/AttendanceRecords.vue"),
+        },
+        {
+          path: "eventRecords",
+          name: "EventRecords",
+          component: () => import("../views/eventRecords/EventRecords.vue"),
+        },
+        {
+          path: "feeCollection",
+          name: "FeeCollection",
+          component: () => import("../views/feeCollection/FeeCollection.vue"),
+        },
+        {
+          path: "feedbackRecords",
+          name: "FeedbackRecords",
+          component: () =>
+            import("../views/feedbackRecords/FeedbackRecords.vue"),
+        },
+        {
+          path: "financialRecords",
+          name: "FinancialRecords",
+          component: () =>
+            import("../views/financialRecords/FinancialRecords.vue"),
+        },
+        {
+          path: "memberManagement",
+          name: "MemberManagement",
+          component: () =>
+            import("../views/memberManagement/MemberManagement.vue"),
+        },
+        {
+          path: "operationRecords",
+          name: "OperationRecords",
+          component: () =>
+            import("../views/operationRecords/OperationRecords.vue"),
+        },
+      ],
     },
   ],
 });
