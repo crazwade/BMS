a0fb4b08cc2da9f5fcbe9a7ac9540691dc8268c9
crazwade <jackywu526@gmail.com>
2023-03-14 17:30:51 +0800

    添加 內容共用元件 HeadTitle

---
+++ src/views/components/HeadTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ props.title }}</h1>
+</template>
+
+<script setup lang="ts">
+interface Title {
+  title: string;
+}
+
+// props data
+const props = defineProps<Title>();
+</script>
