30e71514a027cd30e7e6039db53da5d6fb287835
Jacky <jackywu526@gmail.com>
2023-04-06 15:18:08 +0800

    feat: 製作簡易 404 頁面

---
+++ src/views/pageNotfound/PageNotfound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Page Not Found</h1>
+    <h2>Sorry, the page you are looking for does not exist.</h2>
+    <el-button type="info" @click="goToHomePage">Go to Home Page</el-button>
+  </div>
+</template>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToHomePage = () => {
+  router.push({ path: "/" });
+};
+</script>
