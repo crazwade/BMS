38174b472c1da8c6a5f5519300984a177fa53088
Jacky <jackywu526@gmail.com>
2023-04-06 15:16:36 +0800

    feat: 新增 children path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Layout from "../layout/LayoutView.vue";
 import Login from "../views/login/LoginView.vue";
+import PageNotfound from "../views/pageNotfound/PageNotfound.vue";
+import { usePermissionStore } from "../store/store";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,59 +14,60 @@ const router = createRouter({
     },
     {
       path: "/",
-      redirect: "/home",
-    },
-    {
-      path: "/",
+      redirect: "/HomePage",
       component: Layout,
       name: "Main",
       children: [
         {
-          path: "home",
+          path: "/HomePage",
           name: "HomePage",
-          component: () => import("../views/home/HomePage.vue"),
+          component: () => import("@/views/homePage/HomePage.vue"),
         },
         {
-          path: "attendanceRecords",
-          name: "AttendanceRecords",
-          component: () =>
-            import("../views/attendanceRecords/AttendanceRecords.vue"),
-        },
-        {
-          path: "eventRecords",
-          name: "EventRecords",
-          component: () => import("../views/eventRecords/EventRecords.vue"),
-        },
-        {
-          path: "feeCollection",
-          name: "FeeCollection",
-          component: () => import("../views/feeCollection/FeeCollection.vue"),
-        },
-        {
-          path: "feedbackRecords",
-          name: "FeedbackRecords",
-          component: () =>
-            import("../views/feedbackRecords/FeedbackRecords.vue"),
-        },
-        {
-          path: "financialRecords",
-          name: "FinancialRecords",
-          component: () =>
-            import("../views/financialRecords/FinancialRecords.vue"),
-        },
-        {
-          path: "memberManagement",
+          path: "/MemberManagement",
           name: "MemberManagement",
           component: () =>
-            import("../views/memberManagement/MemberManagement.vue"),
+            import("@/views/memberManagement/MemberManagement.vue"),
         },
         {
-          path: "operationRecords",
+          path: "/AttendanceRecords",
+          name: "AttendanceRecords",
+          component: () =>
+            import("@/views/attendanceRecords/AttendanceRecords.vue"),
+        },
+        {
+          path: "/OperationRecords",
           name: "OperationRecords",
           component: () =>
-            import("../views/operationRecords/OperationRecords.vue"),
+            import("@/views/operationRecords/OperationRecords.vue"),
+        },
+        {
+          path: "/FinancialRecords",
+          name: "FinancialRecords",
+          component: () =>
+            import("@/views/financialRecords/FinancialRecords.vue"),
+        },
+        {
+          path: "/FeeCollection",
+          name: "FeeCollection",
+          component: () => import("@/views/feeCollection/FeeCollection.vue"),
+        },
+        {
+          path: "/EventRecords",
+          name: "EventRecords",
+          component: () => import("@/views/eventRecords/EventRecords.vue"),
+        },
+        {
+          path: "/FeedbackRecords",
+          name: "FeedbackRecords",
+          component: () =>
+            import("@/views/feedbackRecords/FeedbackRecords.vue"),
         },
       ],
+    },
+    {
+      path: "/404",
+      component: PageNotfound,
     },
   ],
 });
