e9adb100f1014e956510bf46d0f656d997d9fe6a
jacky <jackywu526@gmail.com>
2023-08-17 14:32:10 +0800

    feat: 將身分組 memberTypeMap 獨立到 store 共用

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,19 @@
-import { createPinia } from "pinia";
+import { createPinia, defineStore } from "pinia";
 import useUserStore from "./modules/users";
 import usePermissionStore from "./modules/permissions";
 
 const store = createPinia();
+
+export const useMemberStore = defineStore({
+  id: "member",
+  state: () => ({
+    memberTypeMap: new Map([
+      ["normalMember", "一般會員"],
+      ["seniorMember", "高級會員"],
+      ["honoraryMember", "尊榮會員"],
+    ]),
+  }),
+});
 
 export { useUserStore, usePermissionStore };
 
