319ad5ea9d3129f1e5d5baa03f07d1bdf0d025d2
jacky <jackywu526@gmail.com>
2023-08-17 14:31:26 +0800

    feat: 將分頁元件 pagination 獨立出來

---
+++ src/components/PaginationItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-pagination
+    layout="sizes, prev, pager, next"
+    :page-sizes="[10, 20, 30]"
+    :pager-count="5"
+    :page-size="pageSize"
+    :total="total"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    class="justify-end my-3"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  total: number;
+  pageSize: number;
+}>();
+
+const emit = defineEmits(["sizeChange", "currentChange"]);
+
+const handleSizeChange = (newSize: number) => {
+  emit("sizeChange", newSize);
+};
+
+const handleCurrentChange = (newPage: number) => {
+  emit("currentChange", newPage);
+};
+</script>
+
+<style lang="scss" scoped></style>
